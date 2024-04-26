4b13c3f0e6019845d60f3ccebbdb809d16eecd16
dkZzzz <202002020429@stumail.sztu.edu.cn>
2024-04-27 00:28:36 +0800

    add

--- cmd/notice/notice.go
+++ cmd/notice/notice.go
@@ -1,9 +1,49 @@
 package notice
 
 import (
+	"context"
+
+	"github.com/dkZzzz/quality_hub/db/redis"
 	"github.com/dkZzzz/quality_hub/proto/noticepb"
 )
 
 type NoticeServerImpl struct {
 	noticepb.UnimplementedNoticeServer
+}
+
+var (
+	token_error = "token验证失败"
+)
+
+// 具体业务逻辑实现
+func (s *NoticeServerImpl) GetSingleAdvice(ctx context.Context, in *noticepb.GetSingleAdviceReq) (*noticepb.GetSingleAdviceResp, error) {
+	if !redis.JWTMatch(ctx, in.Username, in.Token) {
+		return &noticepb.GetSingleAdviceResp{
+			Code:    401,
+			Message: token_error,
+			Data:    nil,
+		}, nil
+	}
+
+	return &noticepb.GetSingleAdviceResp{
+		Code:    200,
+		Message: "",
+		Data:    nil,
+	}, nil
+}
+
+func (s *NoticeServerImpl) GetProjectAdvice(ctx context.Context, in *noticepb.GetProjectAdviceReq) (*noticepb.GetProjectAdviceResp, error) {
+	if !redis.JWTMatch(ctx, in.Username, in.Token) {
+		return &noticepb.GetProjectAdviceResp{
+			Code:    401,
+			Message: token_error,
+			Data:    nil,
+		}, nil
+	}
+
+	return &noticepb.GetProjectAdviceResp{
+		Code:    200,
+		Message: "",
+		Data:    nil,
+	}, nil
 }
